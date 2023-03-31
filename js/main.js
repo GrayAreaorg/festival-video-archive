2506dcbf5969b255d03f5c9f825ef9f91579302d
Gabriel Dunne <gdunne@quilime.com>
2023-03-31 13:47:04 -0700

    Sort data

--- js/main.js
+++ js/main.js
@@ -1,53 +1,62 @@
 const _festivalData = [
-  './data/2020.json',
-  './data/2021.json',
-  './data/2022.json',
-  './data/2023.json'
+  "./data/2020.json",
+  "./data/2021.json",
+  "./data/2022.json",
+  "./data/2023.json",
 ];
 
 function documentReady(fn) {
-  if (document.readyState !== 'loading') {
+  if (document.readyState !== "loading") {
     fn();
     return;
   }
-  document.addEventListener('DOMContentLoaded', fn);
-  document.addEventListener('alpine:init', () => {
-    Alpine.data('videoData', () => ({
+  document.addEventListener("DOMContentLoaded", fn);
+  document.addEventListener("alpine:init", () => {
+    // get all videos and set as data object
+    Alpine.data("videoData", () => ({
       videos: [],
       async init() {
-        this.videos = await fetchAndCombineFestivalVideoData(_festivalData);
+        this.videos = await fetchAndCombineFestivalData(_festivalData);
       },
-    }))
-  });  
-}  
-documentReady(onReady)
+      sortArrow(col) {
+        return this.sortCol === col ? this.sortArrowIcon : '';
+      },
+      sortCol: '',
+      sort(col) {
+        if (this.sortCol === col) this.sortAsc = !this.sortAsc;
+        this.sortCol = col;
+        this.sortArrowIcon = this.sortAsc ? '▲' : '▼',
+        this.videos.sort((a, b) => {
+          if (a[this.sortCol] < b[this.sortCol]) return this.sortAsc ? 1 : -1;
+          if (a[this.sortCol] > b[this.sortCol]) return this.sortAsc ? -1 : 1;
+          return 0;
+        });
+      },
+    }));
+  });
+}
+documentReady(onReady);
 
 function onReady() {
-  console.info('document ready');
+  console.info("document ready");
 }
 
-function fetchAndCombineFestivalVideoData(urls) {
-  const promises = urls.map(url => {
-    return fetch(url).then(response => response.json());
+function fetchAndCombineFestivalData(urls) {
+  const promises = urls.map((url) => {
+    return fetch(url).then((response) => response.json());
   });
-  return Promise.all(promises)
-    .then(data => {
-      return data.reduce((acc, cur) => {
-        // concat festival metadata onto each video object
-        const videos = cur.videos.map(element => {
-          return {
+  return Promise.all(promises).then((data) => {
+    return data.reduce((acc, cur) => {
+      // concat festival metadata onto each video object
+      const videos = cur.videos.map((element) => {
+        return {
           ...element,
-          "festival" : {
-            "year": cur.year,
-            "description": cur.description,
-            "title": cur.title,
-          }
+          festivalYear: cur.year,
+          festivalDescription: cur.description,
+          festivalTitle: cur.title,
         };
-        });
-        return acc.concat(videos);
-      }, []);
-    });
+      });
+      return acc.concat(videos);
+    }, []);
+  });
 }
-
-
-
