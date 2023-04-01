a4e694d38e63868c676ff742cf1e6644cba29c0a
Gabriel Dunne <gdunne@quilime.com>
2023-03-31 20:33:11 -0700

    Cleanup

--- js/main.js
+++ js/main.js
@@ -1,48 +1,50 @@
 const _databaseFilename = "./data/db.json";
 
-const onReady = () => {
-  console.info("document ready");
-}
+const onReady = () => {};
 
-const filterData = () => {
-  console.log('filter');
-}
-
-const documentReady = (fn) => {
+const DOMContentLoaded = () => {
   if (document.readyState !== "loading") {
-    fn();
+    onReady();
     return;
   }
-  document.addEventListener("DOMContentLoaded", fn);
+  document.addEventListener("DOMContentLoaded", onReady);
   document.addEventListener("alpine:init", () => {
-    // get all videos and set as data object
+    // get videos and set as data object
     Alpine.data("videoData", () => ({
       allVideos: [],
       videos: [],
       async init() {
-        this.videos = await fetch(_databaseFilename).then((response) => response.json());
-        this.sort('festival_year')
+        this.videos = await fetch(_databaseFilename).then((response) =>
+          response.json()
+        );
+        // default sort to festival year
+        this.sort("festival_year");
         this.allVideos = this.videos;
       },
       sortArrow(col) {
-        return this.sortCol === col ? this.sortArrowIcon : '';
+        return this.sortCol === col ? this.sortArrowIcon : "";
       },
+      // default sort to true
       sortAsc: true,
-      sortCol: '',
+      sortCol: "",
       filter(filters) {
         if (filters.length > 0) {
-          this.videos = this.allVideos.filter(video => filters.includes(video.festival_year));
+          this.videos = this.allVideos.filter((video) =>
+            filters.includes(video.festival_year)
+          );
         } else {
           this.videos = this.allVideos;
         }
       },
       sort(col) {
+        const up = "▲";
+        const dn = "▼";
         if (this.sortCol === col) this.sortAsc = !this.sortAsc;
         this.sortCol = col;
-        if (this.sortCol == 'festival_year') {
-          this.sortArrowIcon = this.sortAsc ?  '▼' : '▲';
+        if (this.sortCol == "festival_year") {
+          this.sortArrowIcon = this.sortAsc ? dn : up;
         } else {
-          this.sortArrowIcon = this.sortAsc ? '▲' : '▼';
+          this.sortArrowIcon = this.sortAsc ? up : dn;
         }
         this.videos.sort((a, b) => {
           if (a[this.sortCol] < b[this.sortCol]) return this.sortAsc ? 1 : -1;
@@ -52,6 +54,5 @@ const documentReady = (fn) => {
       },
     }));
   });
-}
-
-documentReady(onReady);
+};
+DOMContentLoaded();
