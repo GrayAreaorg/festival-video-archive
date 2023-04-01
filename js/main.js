aa7511ee1b6a47c8b172f5e969d14c29d6ff8a94
Gabriel Dunne <gdunne@quilime.com>
2023-04-01 11:24:12 -0700

    Move assets to static folder

--- js/main.js
+++ js/main.js
@@ -9,27 +9,21 @@ const DOMContentLoaded = () => {
   }
   document.addEventListener("DOMContentLoaded", onReady);
   document.addEventListener("alpine:init", () => {
-    // get videos and set as data object
     Alpine.data("videoData", () => ({
       allVideos: [],
       videos: [],
       async init() {
-        this.videos = await fetch(_databaseFilename).then((response) =>
-          response.json()
-        );
-        // default sort to festival year
+        this.videos = this.allVideos = await fetch(_databaseFilename).then(response => response.json());
         this.sort("festival_year");
-        this.allVideos = this.videos;
       },
       sortArrow(col) {
         return this.sortCol === col ? this.sortArrowIcon : "";
       },
-      // default sort to true
       sortAsc: true,
       sortCol: "",
       filter(filters) {
         if (filters.length > 0) {
-          this.videos = this.allVideos.filter((video) =>
+          this.videos = this.allVideos.filter(video =>
             filters.includes(video.festival_year)
           );
         } else {
@@ -39,17 +33,17 @@ const DOMContentLoaded = () => {
       sort(col) {
         const up = "▲";
         const dn = "▼";
+
         if (this.sortCol === col) this.sortAsc = !this.sortAsc;
         this.sortCol = col;
-        if (this.sortCol == "festival_year") {
-          this.sortArrowIcon = this.sortAsc ? dn : up;
-        } else {
-          this.sortArrowIcon = this.sortAsc ? up : dn;
-        }
+
+        if (this.sortCol == "festival_year") this.sortArrowIcon = this.sortAsc ? dn : up;
+        else this.sortArrowIcon = this.sortAsc ? up : dn;
+
         this.videos.sort((a, b) => {
           if (a[this.sortCol] < b[this.sortCol]) return this.sortAsc ? 1 : -1;
           if (a[this.sortCol] > b[this.sortCol]) return this.sortAsc ? -1 : 1;
-          return 0;
+          return;
         });
       },
     }));
