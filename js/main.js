f46dc7d206a1d0ba50cd19cd427be2d3ef379010
Gabriel Dunne <gdunne@quilime.com>
2023-04-20 19:22:54 -0700

    Pagination

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 const _databaseFilename = "./data/db.json";
 
-const onReady = () => { };
+const onReady = () => {};
 
-const filterStrings = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"];
+const filterStrings = [
+  "2015",
+  "2016",
+  "2017",
+  "2018",
+  "2019",
+  "2020",
+  "2021",
+  "2022",
+];
 
 const DOMContentLoaded = () => {
   if (document.readyState !== "loading") {
@@ -17,27 +26,30 @@ const DOMContentLoaded = () => {
       selVideo: "",
       sortAsc: true,
       sortCol: "",
+      pageSize: 10,
+      curPage: 1,
       async init() {
-        const _db = await fetch(_databaseFilename).then(
-          (response) => response.json()
+        const _db = await fetch(_databaseFilename).then((response) =>
+          response.json()
         );
         this.videos = this.allVideos = _db.videos;
         this.sort("festival_year");
       },
       fetchSubs(selVideo) {
         if (selVideo.subtitlesFile) {
-          fetch((selVideo.subtitlesFile))
-            .then(response => response.text())
-            .then(data => {
+          fetch(selVideo.subtitlesFile)
+            .then((response) => response.text())
+            .then((data) => {
               selVideo.parsedSubs = data;
             })
-            .catch(error => console.error(error));
+            .catch((error) => console.error(error));
         }
       },
       sortArrow(col) {
         return this.sortCol === col ? this.sortArrowIcon : "";
       },
       filter(filters) {
+        this.curPage = 1;
         if (filters.length > 0) {
           this.videos = this.allVideos.filter((video) =>
             filters.includes(video.festival_year)
@@ -62,6 +74,21 @@ const DOMContentLoaded = () => {
           if (a[this.sortCol] > b[this.sortCol]) return this.sortAsc ? -1 : 1;
           return;
         });
+      },
+      nextPage() {
+        if (this.curPage * this.pageSize < this.videos.length) this.curPage++;
+      },
+      previousPage() {
+        if (this.curPage > 1) this.curPage--;
+      },
+      pagedVideos() {
+        if (this.videos) {
+          return this.videos.filter((row, index) => {
+            let start = (this.curPage - 1) * this.pageSize;
+            let end = this.curPage * this.pageSize;
+            if (index >= start && index < end) return true;
+          });
+        } else return [];
       },
     }));
   });
