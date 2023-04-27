8bb7611a0e31e5383b787d1dc78f1fe491a95aa5
quilime <gdunne@quilime.com>
2023-04-27 12:34:45 -0700

    Caption String search and CSS

--- bin/search-caption-string.sh
+++ bin/search-caption-string.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+
 find ./data/videos -type f -name "*.vtt" -exec grep -l "$1" {} \;
+# find ./data/videos -type f -name "*.vtt" -exec grep -Hl "$1" {} \;
 # find ./data/videos -type f -name "*.vtt" -exec grep -l "$1" {} \; | xargs -I {} basename {}
