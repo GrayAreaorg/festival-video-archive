78f6f8a15006bdbd564903453c2a64b0dc538916
Gabriel Dunne <gdunne@quilime.com>
2023-05-19 22:19:46 -0700

    Update scripts

--- bin/search-caption-string.sh
+++ bin/search-caption-string.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 find ./data/videos -type f -name "*.vtt" -exec grep -l "$1" {} \;
 # find ./data/videos -type f -name "*.vtt" -exec grep -Hl "$1" {} \;
 # find ./data/videos -type f -name "*.vtt" -exec grep -l "$1" {} \; | xargs -I {} basename {}
