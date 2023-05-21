d81a3bd46e413872a894264900dcd509e919820b
Gabriel Dunne <gdunne@quilime.com>
2023-05-20 20:49:17 -0700

    updating sh

--- bin/search-caption-string.sh
+++ bin/search-caption-string.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Query Arrays
-
 # Usage: script.sh sourceDir output.txt query1 [query2 query3 ...]
 
+# Query Arrays
 #   "AI"                  ->  "artificial intelligence" " ai "
 #   "Art"                 ->  " art " "fine art" "gallery"
 #   "Biology"             ->  "biology" "cell" "genetics" "evolution" "physiology" "biochemistry" "adaptation" "reproduction" "biodiversity" "microbio" "molecular"
@@ -18,7 +17,6 @@
 #   "Philosophy"          ->  "philosophy" "metaphysics" "ethics" "aesthetic" "phenomenology"
 #   "Software"            ->  "software" "programming" "code" "algorhythm" "computer science" "open source" "interface"
 #   "Systems"             ->  "systems" "chaos" "complexity" "modeling" "simulation" "pattern"
-
 
 if [[ $# -eq 0 ]]; then
     echo "Usage: $0 sourceDir output.txt query1 [query2 query3 ...]"
@@ -39,9 +37,17 @@ echo "Arguments array: ${argsArray[*]}"
 
 # find each element in the args array
 for pattern in "${argsArray[@]}"; do
+    # find all .vtts and search for the pattern in each one
     find $sourceDir -type f -name "*.vtt" -exec grep -H -n "$pattern" {} \; >> "$outputFile-tmp"
 done
 
+# process temp file and write to output file.
+# 1. `cat "$outputFile-tmp"`: reads contents of temp file
+# 2. `cut -d ':' -f 1`: splits each line using `:` as a delimiter and keep the first field
+# 3. `sort`: sorts the lines alpha/numerically
+# 4. `uniq -c`: removes duplicate lines and add count occurrences for each unique line
+# 5. `sort -r`: reverse sort
+# 6. `> $outputFile`: save to output file
 cat "$outputFile-tmp" | cut -d ':' -f 1 | sort | uniq -c | sort -r > $outputFile
 
 # cleanup
